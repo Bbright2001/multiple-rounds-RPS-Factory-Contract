332da54f31d3393f5fdcee8bf888fc792893acb1
Bright Bassey <basseyu01@gmail.com>
2025-07-09 16:25:43 +0100

    ADD: factory contract logic added

--- src/RPS_Factory_contract.sol
+++ src/RPS_Factory_contract.sol
@@ -7,5 +7,27 @@ contract RPSGame{
 
 
 contract factory{
-    
+    //Array to store deployed child contracts
+    address [] public deployedContracts;
+
+    //event that emits when a contract is deployed
+
+    event contractDeployed(address productContract, address player1);
+
+
+    function createContract(address player1) external returns (address){
+        
+        RPSGame rpsGameInstance = new RPSGame();
+
+        deployedContracts.push(address(rpsGameInstance));
+
+        emit contractDeployed(address(rpsGameInstance), player1);
+
+        return address(rpsGameInstance);
+    }
+
+    function getNumberOfDeployedContract() external view returns (uint256){
+        return deployedContracts.length;
+    }
+
 }
